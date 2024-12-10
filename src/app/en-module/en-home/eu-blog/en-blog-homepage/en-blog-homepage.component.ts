397fc39fd6f60c8cb0bcab603c5502414093d4a1
Dmitriy Morhachov <demm700@gmail.com>
2024-12-10 15:10:27 +0200

    fix

--- src/app/en-module/en-home/eu-blog/en-blog-homepage/en-blog-homepage.component.ts
+++ src/app/en-module/en-home/eu-blog/en-blog-homepage/en-blog-homepage.component.ts
@@ -68,7 +68,7 @@ export class EnBlogHomepageComponent implements OnInit {
   }
   paginatedArticles = []; // Статьи для отображения на текущей странице
   currentPage = 0;
-  pageSize = 9;
+  pageSize = 6;
   onPageChange(event: PageEvent) {
     this.currentPage = event.pageIndex;
     this.pageSize = event.pageSize;
