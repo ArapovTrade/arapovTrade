2180e4f1e29a9fa31beb27d422273ba04419152c
Dmitriy Morhachov <demm700@gmail.com>
2024-12-13 12:49:56 +0200

    8

--- src/app/en-module/en-home/en-trading/eight-en-article/home-en-eight/home-en-eight.component.ts
+++ src/app/en-module/en-home/en-trading/eight-en-article/home-en-eight/home-en-eight.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { ArticlesService } from '../../../../../servises/articles.service';
 
 @Component({
   selector: 'app-home-en-eight',
@@ -7,14 +8,24 @@ import { Meta, Title } from '@angular/platform-browser';
   styleUrl: './home-en-eight.component.scss',
 })
 export class HomeEnEightComponent implements OnInit {
-  constructor(private meta: Meta, private titleService: Title) {}
+  constructor(
+    private meta: Meta,
+    private titleService: Title,
+    private articleServ: ArticlesService
+  ) {}
   ngOnInit(): void {
-    this.titleService.setTitle('Currencies and their quotes  - Arapov.trade');
+    this.titleService.setTitle('Currencies and Their Quotations');
 
     this.meta.updateTag({ name: 'robots', content: 'index, follow' });
     this.meta.addTag({
       name: 'description',
-      content: 'Currencies and their quotes. Basic definitions.',
+      content:
+        'Learn about currencies, their types, features of quotations, and how they are used in international trade and financial markets. A comprehensive guide for traders and investors.',
     });
+    this.gerRandom();
+  }
+  randomArticleRus: any = [];
+  gerRandom() {
+    this.randomArticleRus = this.articleServ.getRandomUkArticles();
   }
 }
--- src/app/ru-module/ru-home/ru-trading/eight-ru-article/home-ru-eight/home-ru-eight.component.ts
+++ src/app/ru-module/ru-home/ru-trading/eight-ru-article/home-ru-eight/home-ru-eight.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { ArticlesService } from '../../../../../servises/articles.service';
 
 @Component({
   selector: 'app-home-ru-eight',
@@ -7,14 +8,24 @@ import { Meta, Title } from '@angular/platform-browser';
   styleUrl: './home-ru-eight.component.scss',
 })
 export class HomeRuEightComponent implements OnInit {
-  constructor(private meta: Meta, private titleService: Title) {}
+  constructor(
+    private meta: Meta,
+    private titleService: Title,
+    private articleServ: ArticlesService
+  ) {}
   ngOnInit(): void {
-    this.titleService.setTitle('Валюты и их котировки  - Arapov.trade');
+    this.titleService.setTitle('Валюты и их котировки');
 
     this.meta.updateTag({ name: 'robots', content: 'index, follow' });
     this.meta.addTag({
       name: 'description',
-      content: 'Валюты и их котировки. Основные определения.',
+      content:
+        'Узнайте, что такое валюты, их виды, особенности котировок и как они используются в международной торговле и финансовых рынках. Полное руководство для трейдеров и инвесторов.',
     });
+    this.gerRandom();
+  }
+  randomArticleRus: any = [];
+  gerRandom() {
+    this.randomArticleRus = this.articleServ.getRandomUkArticles();
   }
 }
