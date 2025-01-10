19d74ec291b5d534f34bab75fcd61eedb0bc98ca
Dmitriy Morhachov <demm700@gmail.com>
2025-01-10 17:27:34 +0200

    52 b

--- src/app/ru-module/ru-home/ru-blog/fifty-two-ru-blog/home-ru-blog-fifty-two/home-ru-blog-fifty-two.component.ts
+++ src/app/ru-module/ru-home/ru-blog/fifty-two-ru-blog/home-ru-blog-fifty-two/home-ru-blog-fifty-two.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { ArticlesService } from '../../../../../servises/articles.service';
 @Component({
   selector: 'app-home-ru-blog-fifty-two',
   templateUrl: './home-ru-blog-fifty-two.component.html',
   styleUrl: './home-ru-blog-fifty-two.component.scss',
 })
 export class HomeRuBlogFiftyTwoComponent implements OnInit {
-  constructor(private meta: Meta, private titleService: Title) {}
+  constructor(
+    private meta: Meta,
+    private titleService: Title,
+    private articleServ: ArticlesService
+  ) {}
   ngOnInit(): void {
     this.titleService.setTitle('Анатомия трендов на рынке - Arapov.trade');
     this.meta.updateTag({ name: 'robots', content: 'index, follow' });
     this.meta.addTag({
       name: 'description',
-      content: 'Анатомия трендов на рынке | Объемный анализ рынка',
+      content:
+        'Разбираем, что такое тренд на финансовом рынке, его фазы, типы и методы анализа. Полное руководство по анатомии трендов: индикаторы, стратегии и принципы торговли.',
     });
+
+    this.gerRandom();
+  }
+  randomArticleRus: any = [];
+  gerRandom() {
+    this.randomArticleRus = this.articleServ.getRandomUkArticles();
   }
 }
--- src/app/uk-module/uk-home/ukblog/fifty-two-uk-blog/home-uk-blog-fifty-two/home-uk-blog-fifty-two.component.ts
+++ src/app/uk-module/uk-home/ukblog/fifty-two-uk-blog/home-uk-blog-fifty-two/home-uk-blog-fifty-two.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { ArticlesService } from '../../../../../servises/articles.service';
 
 @Component({
   selector: 'app-home-uk-blog-fifty-two',
@@ -7,13 +8,24 @@ import { Meta, Title } from '@angular/platform-browser';
   styleUrl: './home-uk-blog-fifty-two.component.scss',
 })
 export class HomeUkBlogFiftyTwoComponent implements OnInit {
-  constructor(private meta: Meta, private titleService: Title) {}
+  constructor(
+    private meta: Meta,
+    private titleService: Title,
+    private articleServ: ArticlesService
+  ) {}
   ngOnInit(): void {
     this.titleService.setTitle('Анатомія трендів на ринку - Arapov.trade');
     this.meta.updateTag({ name: 'robots', content: 'index, follow' });
     this.meta.addTag({
       name: 'description',
-      content: 'Анатомія трендів на ринку | Об`ємний аналіз ринку',
+      content:
+        'Розбираємо, що таке тренд на фінансовому ринку, його фази, типи та методи аналізу. Повний гід з анатомії трендів: індикатори, стратегії та принципи торгівлі.',
     });
+
+    this.gerRandom();
+  }
+  randomArticleRus: any = [];
+  gerRandom() {
+    this.randomArticleRus = this.articleServ.getRandomUkArticles();
   }
 }
