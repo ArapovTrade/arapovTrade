48a5d376f1e8851d575d0daa0b070a00e4a9f5f9
DmitriyMorhachov <demm700@gmail.com>
2025-06-04 17:40:28 +0300

    adding 64-uk-blog microdata

--- src/app/uk-module/uk-home/ukblog/sixty-four-uk-blog/home-uk-blog-sixty-four/home-uk-blog-sixty-four.component.ts
+++ src/app/uk-module/uk-home/ukblog/sixty-four-uk-blog/home-uk-blog-sixty-four/home-uk-blog-sixty-four.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { ArticlesService } from '../../../../../servises/articles.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home-uk-blog-sixty-four',
@@ -11,7 +12,8 @@ export class HomeUkBlogSixtyFourComponent {
   constructor(
     private meta: Meta,
     private titleService: Title,
-    private articleServ: ArticlesService
+    private articleServ: ArticlesService,
+    private route: ActivatedRoute
   ) {}
   ngOnInit(): void {
     this.titleService.setTitle(
@@ -31,6 +33,27 @@ export class HomeUkBlogSixtyFourComponent {
     });
 
     this.gerRandom();
+     this.route.fragment.subscribe((fragment) => {
+      if (fragment) {
+        setTimeout(() => {
+          const element = document.getElementById(fragment);
+          if (element) {
+            // Отступ сверху в пикселях, например 80px (зависит от вашего меню)
+            const offset = 80;
+
+            // Позиция элемента относительно страницы
+            const elementPosition =
+              element.getBoundingClientRect().top + window.pageYOffset;
+
+            // Скроллим с учётом отступа
+            window.scrollTo({
+              top: elementPosition - offset,
+              behavior: 'smooth',
+            });
+          }
+        }, 100);
+      }
+    });
   }
   randomArticleRus: any = [];
   gerRandom() {
