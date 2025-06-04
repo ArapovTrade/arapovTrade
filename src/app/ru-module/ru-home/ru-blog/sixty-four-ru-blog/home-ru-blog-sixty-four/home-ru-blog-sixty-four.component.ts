b21cf2c021c4a78e3173065d76ddb3d1d19a297f
DmitriyMorhachov <demm700@gmail.com>
2025-06-04 16:43:34 +0300

    adding microdate to practik page

--- src/app/ru-module/ru-home/ru-blog/sixty-four-ru-blog/home-ru-blog-sixty-four/home-ru-blog-sixty-four.component.ts
+++ src/app/ru-module/ru-home/ru-blog/sixty-four-ru-blog/home-ru-blog-sixty-four/home-ru-blog-sixty-four.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { ArticlesService } from '../../../../../servises/articles.service';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home-ru-blog-sixty-four',
   templateUrl: './home-ru-blog-sixty-four.component.html',
@@ -11,7 +11,8 @@ export class HomeRuBlogSixtyFourComponent {
   constructor(
     private meta: Meta,
     private titleService: Title,
-    private articleServ: ArticlesService
+    private articleServ: ArticlesService,
+    private route: ActivatedRoute
   ) {}
   ngOnInit(): void {
     this.titleService.setTitle(
@@ -31,6 +32,28 @@ export class HomeRuBlogSixtyFourComponent {
     });
 
     this.gerRandom();
+
+    this.route.fragment.subscribe((fragment) => {
+      if (fragment) {
+        setTimeout(() => {
+          const element = document.getElementById(fragment);
+          if (element) {
+            // Отступ сверху в пикселях, например 80px (зависит от вашего меню)
+            const offset = 80;
+
+            // Позиция элемента относительно страницы
+            const elementPosition =
+              element.getBoundingClientRect().top + window.pageYOffset;
+
+            // Скроллим с учётом отступа
+            window.scrollTo({
+              top: elementPosition - offset,
+              behavior: 'smooth',
+            });
+          }
+        }, 100);
+      }
+    });
   }
   randomArticleRus: any = [];
   gerRandom() {
