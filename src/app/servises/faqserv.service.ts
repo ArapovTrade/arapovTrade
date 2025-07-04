6f5185ea109c0a1a7180881f1d1e5223a83d2f81
DmitriyMorhachov <demm700@gmail.com>
2025-07-04 21:05:05 +0300

    fix

--- src/app/servises/faqserv.service.ts
+++ src/app/servises/faqserv.service.ts
@@ -1,5 +1,5 @@
-import { Injectable, Inject } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { Injectable } from '@angular/core';
+ 
 import { FaqRuAuthorService } from './faqservices/faq-ru-author.service';
 import { FaqRuCourseService } from './faqservices/faq-ru-course.service';
 
@@ -23,7 +23,7 @@ export class FaqservService {
   languageForFAQ = 'uk';
 
   constructor(
-    @Inject(DOCUMENT) private document: Document,
+    
     private faqRuAut: FaqRuAuthorService,
 
     private faqUkAut: FaqUkAuthorService,
@@ -36,29 +36,7 @@ export class FaqservService {
   ) {}
 
   returnSchema(langcode: string, path: string) {
-    // if(path==''){
-    //   this.faqs=this.faqUkAut.getFaq()
-    // }else if(langcode == 'uk'&&path=='uk'){
-    //    this.faqs=this.faqUkAut.getFaq()
-    // }else if(langcode == 'ru'&&path=='ru'){
-    //    this.faqs=this.faqRuAut.getFaq()
-    // }else if(langcode == 'ru'&&path=='ru/studying'){
-    //    this.faqs=this.faqRuCrs.getFaq()
-    // }
-    // else if(langcode == 'uk'&&path=='uk/studying'){
-    //    this.faqs=this.faqUkCrs.getFaq()
-    // }
-    // else if(langcode == 'uk'&&path=='uk/freestudying/freeeducation'){
-    //    this.faqs=this.faqUkOwl.getFaq()
-    // }
-    // else if(langcode == 'ru'&&path=='ru/freestudying/freeeducation'){
-    //    this.faqs=this.faqRuOwl.getFaq()
-    // }
-    // else if(langcode == 'ru'){
-    //    this.faqs=this.faqRuPages.getFaq()
-    // }else{
-    //   this.faqs=this.faqUkPages.getFaq()
-    // }
+    
 
     this.faqs =
       path === '' || (langcode === 'uk' && path === 'uk')
