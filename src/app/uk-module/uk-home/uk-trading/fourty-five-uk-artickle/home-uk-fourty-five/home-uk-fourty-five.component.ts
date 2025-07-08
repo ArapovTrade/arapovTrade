b6c2556aff1ff4ca28c2e64864ed2d621e8b141f
DmitriyMorhachov <demm700@gmail.com>
2025-07-08 18:04:33 +0300

    adding howTo meta tags

--- src/app/uk-module/uk-home/uk-trading/fourty-five-uk-artickle/home-uk-fourty-five/home-uk-fourty-five.component.ts
+++ src/app/uk-module/uk-home/uk-trading/fourty-five-uk-artickle/home-uk-fourty-five/home-uk-fourty-five.component.ts
@@ -33,7 +33,7 @@ export class HomeUkFourtyFiveComponent implements OnInit {
       property: 'og:image',
       content: '/assets/img/content/freeeducationnew.webp',
     });
-
+     this.addJsonLdScript();
     this.gerRandom();
 
     this.route.fragment.subscribe((fragment) => {
@@ -65,6 +65,88 @@ export class HomeUkFourtyFiveComponent implements OnInit {
       
 
   }
+
+  private addJsonLdScript(): void {
+    const jsonLdScript = this.document.createElement('script');
+    jsonLdScript.type = 'application/ld+json';
+    jsonLdScript.text = JSON.stringify({
+      '@context': 'https://schema.org',
+      '@type': 'HowTo',
+      name: 'Як пройти безкоштовний курс із трейдингу від Ігоря Арапова',
+      description:
+        'Покрокова інструкція для самостійного проходження безкоштовного онлайн-курсу трейдингу.',
+      author: { '@type': 'Person', name: 'Ігор Арапов' },
+      publisher: {
+        '@type': 'Organization',
+        name: 'Ігор Арапов',
+        logo: { '@type': 'ImageObject', url: 'https://arapov.trade/favicon.ico' },
+      },
+      mainEntityOfPage: {
+        '@type': 'WebPage',
+        '@id': 'https://arapov.trade/uk/freestudying/freeeducation',
+      },
+      step: [
+        {
+          '@type': 'HowToStep',
+          name: 'Ознайомтеся з програмою курсу',
+          text: 'Перейдіть до розділу «Програма курсу» та вивчіть, з чого складається навчання.',
+          url: 'https://arapov.trade/uk/freestudying/freeeducation#section1',
+        },
+        {
+          '@type': 'HowToStep',
+          name: 'Пройдіть модуль «Трейдинг для початківців»',
+          text: 'Розберіться з базовими поняттями та основами біржової торгівлі.',
+          url: 'https://arapov.trade/uk/freestudying/freeeducation#section2',
+        },
+        {
+          '@type': 'HowToStep',
+          name: 'Вивчіть технічний аналіз',
+          text: 'Опанувати базові патерни поведінки ціни, фігури розвороту, рівні.',
+          url: 'https://arapov.trade/uk/freestudying/freeeducation#section3',
+        },
+        {
+          '@type': 'HowToStep',
+          name: 'Поглибіться в об’ємний аналіз',
+          text: 'Навчіться читати біржові обсяги та познайомтеся з концепцією Річарда Вайкоффа.',
+          url: 'https://arapov.trade/uk/freestudying/freeeducation#section4',
+        },
+        {
+          '@type': 'HowToStep',
+          name: 'Опанувати стратегію Smart Money',
+          text: 'Дізнайтесь, як працюють великі учасники ринку та як слідувати за ними.',
+          url: 'https://arapov.trade/uk/freestudying/freeeducation#section5',
+        },
+        {
+          '@type': 'HowToStep',
+          name: 'Вивчіть психологію трейдингу',
+          text: 'Розберіться, як контролювати емоції та мислити як професіонал.',
+          url: 'https://arapov.trade/uk/freestudying/freeeducation#section6',
+        },
+        {
+          '@type': 'HowToStep',
+          name: 'Проаналізуйте торгові приклади',
+          text: 'Розбір угод допоможе зрозуміти, як застосовувати теорію на практиці.',
+          url: 'https://arapov.trade/uk/freestudying/freeeducation#section9',
+        },
+        {
+          '@type': 'HowToStep',
+          name: 'Ознайомтеся з часто заданими питаннями',
+          text: 'Відповіді на поширені питання щодо курсу та трейдингу.',
+          url: 'https://arapov.trade/uk/freestudying/freeeducation#section11',
+        },
+        {
+          '@type': 'HowToStep',
+          name: 'Підведіть підсумки курсу',
+          text: 'Зробіть для себе ключові висновки та підготуйтеся до самостійної торгівлі.',
+          url: 'https://arapov.trade/uk/freestudying/freeeducation#section10',
+        },
+      ],
+    });
+    this.document.head.appendChild(jsonLdScript);
+  }
+
+
+
   randomArticleRus: any = [];
   gerRandom() {
     this.randomArticleRus = this.articleServ.getRandomUkArticles();
