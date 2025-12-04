3dc6800777b3720862a2df87005edc0b5e4594ab
DmitriyMorhachov <demm700@gmail.com>
2025-12-04 18:42:13 +0200

    fix robots sitemap

--- src/app/ru-module/ru-home/ru-blog/twenty-eight-ru-blog/twenty-eight-ru-blog.module.ts
+++ src/app/ru-module/ru-home/ru-blog/twenty-eight-ru-blog/twenty-eight-ru-blog.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRuBlogTwentyEightComponent } from './home-ru-blog-twenty-eight/home-ru-blog-twenty-eight.component';
-
+import { MatExpansionModule } from '@angular/material/expansion';
 import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', component: HomeRuBlogTwentyEightComponent },
@@ -9,6 +9,6 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [HomeRuBlogTwentyEightComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+    imports: [CommonModule,MatExpansionModule, RouterModule.forChild(routes)],
 })
 export class TwentyEightRuBlogModule {}
