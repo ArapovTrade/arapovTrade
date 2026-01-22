7f766686f38cd85564be689ae36b272be017b40a
DmitriyMorhachov <demm700@gmail.com>
2026-01-22 14:45:12 +0200

    three artickles

--- src/app/en-module/en-home/en-trading/eight-en-article/eight-en-article.module.ts
+++ src/app/en-module/en-home/en-trading/eight-en-article/eight-en-article.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeEnEightComponent } from './home-en-eight/home-en-eight.component';
 import { Routes, RouterModule } from '@angular/router';
-
+import { MatExpansionModule } from '@angular/material/expansion';
 const routes: Routes = [{ path: '', component: HomeEnEightComponent }];
 @NgModule({
   declarations: [HomeEnEightComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+    imports: [CommonModule,MatExpansionModule, RouterModule.forChild(routes)],
 })
 export class EightEnArticleModule {}
