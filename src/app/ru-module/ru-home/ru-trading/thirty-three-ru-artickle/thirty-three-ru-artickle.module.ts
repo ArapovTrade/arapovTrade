99dce9a4586c3a3d72148c0a9abb52cf1736620b
DmitriyMorhachov <demm700@gmail.com>
2026-01-28 15:00:12 +0200

    one +

--- src/app/ru-module/ru-home/ru-trading/thirty-three-ru-artickle/thirty-three-ru-artickle.module.ts
+++ src/app/ru-module/ru-home/ru-trading/thirty-three-ru-artickle/thirty-three-ru-artickle.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRuThirtyThreeComponent } from './home-ru-thirty-three/home-ru-thirty-three.component';
-
+import { MatExpansionModule } from '@angular/material/expansion';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [{ path: '', component: HomeRuThirtyThreeComponent }];
 @NgModule({
   declarations: [HomeRuThirtyThreeComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule,MatExpansionModule, RouterModule.forChild(routes)],
 })
 export class ThirtyThreeRuArtickleModule {}
