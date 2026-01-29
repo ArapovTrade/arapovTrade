b9e2f3acb6f0ce8a29536636a5ddb0af690cf25e
DmitriyMorhachov <demm700@gmail.com>
2026-01-29 16:26:41 +0200

    one

--- src/app/uk-module/uk-home/uk-trading/thirty-six-uk-artickle/thirty-six-uk-artickle.module.ts
+++ src/app/uk-module/uk-home/uk-trading/thirty-six-uk-artickle/thirty-six-uk-artickle.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeUkThirtySixComponent } from './home-uk-thirty-six/home-uk-thirty-six.component';
-
+import { MatExpansionModule } from '@angular/material/expansion';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [{ path: '', component: HomeUkThirtySixComponent }];
 @NgModule({
   declarations: [HomeUkThirtySixComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+ imports: [CommonModule,MatExpansionModule, RouterModule.forChild(routes)],
 })
 export class ThirtySixUkArtickleModule {}
