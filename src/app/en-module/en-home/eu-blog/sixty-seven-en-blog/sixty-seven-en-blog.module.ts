f7d52ed8844064a75d5de0ed4424d7dbce526d0a
DmitriyMorhachov <demm700@gmail.com>
2026-02-02 16:01:04 +0200

    one+

--- src/app/en-module/en-home/eu-blog/sixty-seven-en-blog/sixty-seven-en-blog.module.ts
+++ src/app/en-module/en-home/eu-blog/sixty-seven-en-blog/sixty-seven-en-blog.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeEnBlogSixtySevenComponent } from './home-en-blog-sixty-seven/home-en-blog-sixty-seven.component';
-
+import { MatExpansionModule } from '@angular/material/expansion';
 import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [{ path: '', component: HomeEnBlogSixtySevenComponent }];
 
 @NgModule({
   declarations: [HomeEnBlogSixtySevenComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule,MatExpansionModule, RouterModule.forChild(routes)],
+
 })
 export class SixtySevenEnBlogModule {}
