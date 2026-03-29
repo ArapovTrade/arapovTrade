5dc8a82ff976483b75a8d6626f61e4ee314dbee4
DmitriyMorhachov <demm700@gmail.com>
2026-03-29 16:38:57 +0300

    one more artickle 90b

--- src/app/ru-module/ru-home/ru-blog/ninty-ru-blog/ninty-ru-blog.module.ts
+++ src/app/ru-module/ru-home/ru-blog/ninty-ru-blog/ninty-ru-blog.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRuBlogNintyComponent } from './home-ru-blog-ninty/home-ru-blog-ninty.component';
-
+import { MatExpansionModule } from '@angular/material/expansion';
 
 
 import { Routes, RouterModule } from '@angular/router';
@@ -9,6 +9,6 @@ const routes: Routes = [{ path: '', component: HomeRuBlogNintyComponent }];
 
 @NgModule({
   declarations: [HomeRuBlogNintyComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule,MatExpansionModule, RouterModule.forChild(routes)],
 })
 export class NintyRuBlogModule { }
--- src/app/uk-module/uk-home/ukblog/ninty-uk-blog/ninty-uk-blog.module.ts
+++ src/app/uk-module/uk-home/ukblog/ninty-uk-blog/ninty-uk-blog.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeUkBlogNintyComponent } from './home-uk-blog-ninty/home-uk-blog-ninty.component';
-
+import { MatExpansionModule } from '@angular/material/expansion';
 
 
 import { RouterModule, Routes } from '@angular/router';
@@ -9,6 +9,7 @@ const routes: Routes = [{ path: '', component: HomeUkBlogNintyComponent }];
 
 @NgModule({
   declarations: [HomeUkBlogNintyComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+ imports: [CommonModule,MatExpansionModule, RouterModule.forChild(routes)],
+
 })
 export class NintyUkBlogModule { }
