c4ee037ee54aa52c03a46b443ec956faeb2f011a
DmitriyMorhachov <demm700@gmail.com>
2026-04-03 17:11:44 +0300

    93   b

--- src/app/en-module/en-home/eu-blog/ninty-three-en-blog/ninty-three-en-blog.module.ts
+++ src/app/en-module/en-home/eu-blog/ninty-three-en-blog/ninty-three-en-blog.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeEnBlogNintyThreeComponent } from './home-en-blog-ninty-three/home-en-blog-ninty-three.component';
-
+import { MatExpansionModule } from '@angular/material/expansion';
 
 
 import { RouterModule, Routes } from '@angular/router';
@@ -9,6 +9,6 @@ const routes: Routes = [{ path: '', component: HomeEnBlogNintyThreeComponent }];
 
 @NgModule({
   declarations: [HomeEnBlogNintyThreeComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+   imports: [CommonModule,MatExpansionModule, RouterModule.forChild(routes)],
 })
 export class NintyThreeEnBlogModule { }
